7b35a067b02330470656fa644d3d960207fd0e0f
rhit-cerasoml <90203329+rhit-cerasoml@users.noreply.github.com>
2023-01-11 14:11:38 -0500

    Update create_table_person.sql

--- create_table_person.sql
+++ create_table_person.sql
@@ -2,12 +2,11 @@ use SymptomTracker
 GO
 
 CREATE TABLE Person (
-	ID Integer Identity(1, 1),
+	ID Integer Identity(1, 1) PRIMARY KEY,
 	fname varchar(30),
 	lname varchar(30),
-	login varchar(30),
+	login varchar(30) UNIQUE,
 	password varchar(30),
 	role char(2),
-	hcpID Integer REFERENCES dbo.HealthCareProvider,
-	CONSTRAINT primary_key PRIMARY KEY (ID, login)
+	hcpID Integer REFERENCES dbo.HealthCareProvider
 )
