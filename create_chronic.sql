604aeb98c44d1eafe4fb1b067fe50ef3ff80f429
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-01-13 08:15:29 -0500

    Update create_chronic.sql

--- create_chronic.sql
+++ create_chronic.sql
@@ -1,8 +1,11 @@
 use SymptomTracker
 go
-
 create table Chronic(
-	PersonID Integer REFERENCES dbo.Person(ID),
-	SymptomID Integer REFERENCES dbo.Symptom(ID),
+	PersonID Integer REFERENCES dbo.Person(ID)
+		On Delete Cascade
+		On Update Cascade,
+	SymptomID Integer REFERENCES dbo.Symptom(ID)
+		On Delete Cascade
+		On Update Cascade,
 	primary key (PersonID, SymptomID)
 );
