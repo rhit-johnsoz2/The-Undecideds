1d86d0b1215886dd5930f2515d75ebe2b89ecb79
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-01-19 17:15:59 -0500

    Create create_view_doctor.sql

---
+++ create_view_doctor.sql
@@ -0,0 +1,9 @@
+Use SymptomTracker
+Go
+CREATE VIEW DoctorView
+AS
+SELECT Patient.fname, Patient.lname, Patient.ID
+FROM Person Doctor JOIN DoctorFor DF
+On Doctor.ID = DF.doctorID
+JOIN Person Patient on DF.patientID = Patient.ID
+WHERE Doctor.role = 1
