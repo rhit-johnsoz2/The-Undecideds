de9ec234db0ba737a2ee3ca254f0f79b7a257453
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-01-20 02:51:37 -0500

    Update insert_insures.sql

--- insert_insures.sql
+++ insert_insures.sql
@@ -21,14 +21,39 @@ BEGIN
 	IF(NOT EXISTS (SELECT * FROM HealthCareProvider WHERE ID = @hcpID))
 	BEGIN
 		RAISERROR('Health Care Provider not in the HCP database', 14, 1)
-		Return 2
+		Return 3
 	END
 	-- check to see if the treatment exists in the treatment database
 	IF(NOT EXISTS (SELECT * FROM Treatment WHERE ID = @treatmentID))
 	BEGIN
 		RAISERROR('Treatment not in the treatment database', 14, 1)
-		Return 2
+		Return 4
 	END
 INSERT INTO Insures Values(@personID, @hcpID, @treatmentID, @coverage)
 Return 0
 END
+
+--Testing no errors
+--DECLARE @status int
+--EXEC @status = dbo.InsertInsures @personID = 2, @hcpID = 1, @treatmentID = 2, @coverage = 5.03
+--SELECT @status
+
+--Testing error code 1
+--DECLARE @status int
+--EXEC @status = dbo.InsertInsures @personID = 2, @hcpID = null, @treatmentID = 2, @coverage = 5.03
+--SELECT @status
+
+--Testing error code 2
+--DECLARE @status int
+--EXEC @status = dbo.InsertInsures @personID = 50, @hcpID = 1, @treatmentID = 2, @coverage = 5.03
+--SELECT @status
+
+--Testing error code 3
+--DECLARE @status int
+--EXEC @status = dbo.InsertInsures @personID = 2, @hcpID = 20, @treatmentID = 2, @coverage = 5.03
+--SELECT @status
+
+--Testing error code 4
+--DECLARE @status int
+--EXEC @status = dbo.InsertInsures @personID = 2, @hcpID = 1, @treatmentID = 30, @coverage = 5.03
+--SELECT @status
