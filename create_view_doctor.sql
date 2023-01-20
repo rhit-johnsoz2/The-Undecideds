0cae19101b97cf10728a60c81893075612621850
rhit-cerasoml <90203329+rhit-cerasoml@users.noreply.github.com>
2023-01-20 02:03:15 -0500

    Update create_view_doctor.sql

--- create_view_doctor.sql
+++ create_view_doctor.sql
@@ -6,4 +6,4 @@ SELECT Patient.fname, Patient.lname, Patient.ID
 FROM Person Doctor JOIN DoctorFor DF
 On Doctor.ID = DF.doctorID
 JOIN Person Patient on DF.patientID = Patient.ID
-WHERE Doctor.role = 1
+--WHERE Doctor.role = 1
