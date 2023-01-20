a36f9337324467f1af40db8e4112d359dbfd39df
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-01-20 01:51:44 -0500

    Update insert_health_care_provider.sql

--- insert_health_care_provider.sql
+++ insert_health_care_provider.sql
@@ -1,21 +1,36 @@
 Use SymptomTracker
 Go
 CREATE PROCEDURE InsertHealthCareProviders
-	(@ID int, @name varchar(50))
+	(@name varchar(50))
 AS
 BEGIN
 	-- check to see if any of the parameters are null
-	IF(@ID is NULL or @name is NULL)
+	IF(@name is NULL)
 	BEGIN
 		RAISERROR('Input arguments cannot be null', 14, 1)
 		Return 1
 	END
 	-- check to see if the hcp already exists in the hcp database
-	IF(EXISTS (SELECT * FROM HealthCareProvider WHERE ID = @ID))
+	IF(EXISTS (SELECT * FROM HealthCareProvider WHERE name = @name))
 	BEGIN
 		RAISERROR('HCP already in HCP database', 14, 1)
 		Return 2
 	END
-INSERT INTO HealthCareProvider Values(@ID, @name)
+INSERT INTO HealthCareProvider Values(@name)
 Return 0
 END
+
+--Testing no errors
+--DECLARE @status int
+--EXEC @status = dbo.InsertHealthCareProviders @name = 'Terre Haute Hospital'
+--SELECT @status
+
+--Testing error code 1
+--DECLARE @status int
+--EXEC @status = dbo.InsertHealthCareProviders @name = null
+--SELECT @status
+
+--Testing error code 2
+--DECLARE @status int
+--EXEC @status = dbo.InsertHealthCareProviders @name = 'Terre Haute Hospital'
+--SELECT @status
