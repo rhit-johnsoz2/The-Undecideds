bb379804e501fc9f2a4e86cfcec0a20cb614b705
rhit-cerasoml <90203329+rhit-cerasoml@users.noreply.github.com>
2023-01-20 02:08:42 -0500

    Update create_view_doctor.sql

--- create_view_doctor.sql
+++ create_view_doctor.sql
@@ -6,4 +6,4 @@ SELECT Doctor.ID AS DoctorID, Patient.fname, Patient.lname, Patient.ID
 FROM Person Doctor JOIN DoctorFor DF
 On Doctor.ID = DF.doctorID
 JOIN Person Patient on DF.patientID = Patient.ID
---WHERE Doctor.role = 1
+WHERE Doctor.role = 'DR' AND Patient.role = 'PA'
