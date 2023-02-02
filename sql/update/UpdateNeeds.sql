fd9840ab5d731b800303f8fb633a09fea9b9d83c
rhit-fogartee <78517971+rhit-fogartee@users.noreply.github.com>
2023-02-02 17:33:08 -0500

    Delete UpdateNeeds.sql

--- sql/update/UpdateNeeds.sql
+++
@@ -1,8 +0,0 @@
-create procedure UpdateNeeds(@PersonId int, @Treatment int, @Start date, @end date)
-As
-Begin
-	Update Needs
-	Set SDate = @Start,
-		EDate = @end
-	Where PatientID = @PersonId and TreatmentID = @Treatment
-End
