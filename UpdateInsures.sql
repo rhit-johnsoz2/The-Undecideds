4367a028fcedd46fcd514c2e15ba0c53eebae202
rhit-fogartee <78517971+rhit-fogartee@users.noreply.github.com>
2023-02-02 17:28:19 -0500

    Update and rename UpdateInsures.sql to update_insures.sql

--- UpdateInsures.sql
+++
@@ -1,7 +0,0 @@
-create procedure UpdateInsures(@PersonId int, @HCPID int, @Treatment int, @newCost int)
-As
-Begin
-	Update Insures
-	Set Coverage = @newCost
-	Where PersonID = @PersonId and HCPID = @HCPID and TreatmentID = @Treatment
-End
