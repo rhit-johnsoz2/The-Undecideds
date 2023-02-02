4514c67d9c053969a97560b374c9b99f36fec7e9
rhit-fogartee <78517971+rhit-fogartee@users.noreply.github.com>
2023-02-02 17:11:24 -0500

    Update ClientWindow.java

--- Client/src/com/undecideds/ui/ClientWindow.java
+++ Client/src/com/undecideds/ui/ClientWindow.java
@@ -1,143 +1,131 @@
 package com.undecideds.ui;
 
 import com.undecideds.services.InsertServiceList;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.sql.Date;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoField;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
-public class ClientWindow implements ActionListener{
+public class ClientWindow implements ActionListener {
     JPanel panel;
-    JLabel user_label, password_label, message;
+    JLabel user_label;
+    JLabel password_label;
+    JLabel message;
     JTextField userName_text;
     JPasswordField password_text;
-    JButton submit, cancel;
-    public void launch(){
-            // Username Label
-            user_label = new JLabel();
-            user_label.setText("User Name :");
-            userName_text = new JTextField();
-            // Password Label
-            password_label = new JLabel();
-            password_label.setText("Password :");
-            password_text = new JPasswordField();
-            // Submit
-            submit = new JButton("SUBMIT");
-            panel = new JPanel(new GridLayout(3, 1));
-            panel.add(user_label);
-            panel.add(userName_text);
-            panel.add(password_label);
-            panel.add(password_text);
-            message = new JLabel();
-            panel.add(message);
-            panel.add(submit);
-            // Adding the listeners to components..
-            submit.addActionListener(this);
-            JFrame frame = new JFrame();
-            frame.add(panel, BorderLayout.CENTER);
-            frame.setTitle("Please Login Here !");
-            frame.setSize(450,350);
-            frame.setVisible(true);
+    JButton submit;
+    JFrame frame = new JFrame();
+
+    public ClientWindow() {
+    }
+
+    public void launch() {
+        this.user_label = new JLabel();
+        this.user_label.setText("User Name :");
+        this.userName_text = new JTextField();
+        this.password_label = new JLabel();
+        this.password_label.setText("Password :");
+        this.password_text = new JPasswordField();
+        this.submit = new JButton("SUBMIT");
+        this.panel = new JPanel(new GridLayout(5, 1));
+        this.panel.add(this.user_label);
+        this.panel.add(this.userName_text);
+        this.panel.add(this.password_label);
+        this.panel.add(this.password_text);
+        this.message = new JLabel();
+        this.panel.add(this.message);
+        this.panel.add(this.submit);
+        this.submit.addActionListener(this);
+        this.frame.add(this.panel, "Center");
+        this.frame.setTitle("Please Login Here !");
+        this.frame.setSize(500, 300);
+        this.frame.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        String userName = this.userName_text.getText();
+        String password = this.password_text.getText();
+        if (userName.trim().equals("admin") && password.trim().equals("admin")) {
+            this.message.setText(" Hello " + userName);
+            this.DoctorPage();
+            this.frame.dispatchEvent(new WindowEvent(this.frame, 201));
+        } else {
+            this.message.setText(" Invalid user.. ");
         }
 
-        @Override
-        public void actionPerformed(ActionEvent ae) {
-            String userName = userName_text.getText();
-            String password = password_text.getText();
-            if (userName.trim().equals("admin") && password.trim().equals("admin")) {
-                message.setText(" Hello " + userName + "");
-//                this.PatientPage();
-                this.DoctorPage();
-            } else {
-                message.setText(" Invalid user.. ");
-            }
-        }
-    public void PatientPage(){
+    }
+
+    public void PatientPage() {
         JFrame frame = new JFrame();
-        frame.setSize(500,300);
+        frame.setSize(500, 300);
         frame.setTitle("Symptom Tracker: Patient View");
-
-//        /* Add Acute */
-        String[] symptoms = {"Headache", "Cold"};
-        JComboBox<String> symptom = new JComboBox<>(symptoms);
-        String[] severities = {"1","2","3","4","5","6","7","8","9","10"};
-        JComboBox<String> severity = new JComboBox<>(severities);
+        String[] symptoms = new String[]{"Headache", "Cold"};
+        JComboBox<String> symptom = new JComboBox(symptoms);
+        String[] severities = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        JComboBox<String> severity = new JComboBox(severities);
         JButton submit = new JButton("Submit");
-        submit.addActionListener(e -> {
-            String selectedSymptom = "You selected " + symptom.getItemAt(symptom.getSelectedIndex());
-            String selectedSeverity = "You selected " + severity.getItemAt(severity.getSelectedIndex());
+        submit.addActionListener((e) -> {
+            String selectedSymptom = "You selected " + (String)symptom.getItemAt(symptom.getSelectedIndex());
+            String selectedSeverity = "You selected " + (String)severity.getItemAt(severity.getSelectedIndex());
             System.out.println(selectedSymptom);
             System.out.println(selectedSeverity);
-
-            int result = InsertServiceList.INSERT_ACUTE.ExecuteQuery(new Object[]{
-                    symptom.getItemAt(symptom.getSelectedIndex()),
-                    severity.getItemAt(severity.getSelectedIndex()),
-                    LocalDateTime.now()
-            });
+            int result = InsertServiceList.INSERT_ACUTE.ExecuteQuery(new Object[]{symptom.getItemAt(symptom.getSelectedIndex()), severity.getItemAt(severity.getSelectedIndex()), LocalDateTime.now()});
             System.out.println(InsertServiceList.INSERT_ACUTE.codeMeaning(result));
         });
-
-
-
-//        /* Add Chronic */
-        String[] chronicSymptoms = {"Cough"};
-        JComboBox<String> chronicSymptom = new JComboBox<>(chronicSymptoms);
+        String[] chronicSymptoms = new String[]{"Cough"};
+        JComboBox<String> chronicSymptom = new JComboBox(chronicSymptoms);
         JButton submit2 = new JButton("Submit");
-        submit2.addActionListener(e -> {
-
-            int result = InsertServiceList.INSERT_CHRONIC.ExecuteQuery(new Object[]{
-                    chronicSymptom.getItemAt(chronicSymptom.getSelectedIndex()),
-                    LocalDateTime.now()
-            });
+        submit2.addActionListener((e) -> {
+            int result = InsertServiceList.INSERT_CHRONIC.ExecuteQuery(new Object[]{chronicSymptom.getItemAt(chronicSymptom.getSelectedIndex()), LocalDateTime.now()});
             System.out.println(InsertServiceList.INSERT_CHRONIC.codeMeaning(result));
         });
-
-//        /* Symptom Table */
-        JTable j;
-        String[][] data = {
-                { "Cough", "3", "10:00:00PM" },
-                { "Cough", "5", "10:00:00AM" },
-                { "Cough", "8", "10:00:00PM" }
-        };
-        String[] columnNames = { "Name", "Roll Number", "Department" };
-        j = new JTable(data, columnNames);
+        String[][] data = new String[][]{{"Cough", "3", "10:00:00PM"}, {"Cough", "5", "10:00:00AM"}, {"Cough", "8", "10:00:00PM"}};
+        String[] columnNames = new String[]{"Name", "Roll Number", "Department"};
+        JTable j = new JTable(data, columnNames);
         j.setBounds(50, 150, 200, 50);
         JLabel TableTitle = new JLabel("Recent Symptoms");
         TableTitle.setBounds(50, 125, 140, 20);
-
         JLabel header = new JLabel("Patient View");
         JPanel Panel = new JPanel();
-        Panel.setLayout(new BoxLayout(Panel, BoxLayout.X_AXIS));
+        Panel.setLayout(new BoxLayout(Panel, 0));
         JPanel ChronicPanel = new JPanel();
-        ChronicPanel.setLayout(new BoxLayout(ChronicPanel, BoxLayout.X_AXIS));
-
+        ChronicPanel.setLayout(new BoxLayout(ChronicPanel, 0));
         GridBagLayout layout = new GridBagLayout();
         Panel.setLayout(layout);
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = 2;
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        Panel.add(header,gbc);
-
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        Panel.add(header, gbc);
+        gbc.fill = 2;
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        Panel.add(symptom,gbc);
+        Panel.add(symptom, gbc);
         gbc.ipady = 20;
         gbc.gridx = 1;
         gbc.gridy = 1;
-        Panel.add(severity,gbc);
+        Panel.add(severity, gbc);
         gbc.ipady = 20;
         gbc.gridx = 2;
         gbc.gridy = 1;
         Panel.add(submit, gbc);
-
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.fill = 2;
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 2;
@@ -145,81 +133,106 @@ public class ClientWindow implements ActionListener{
         gbc.ipady = 20;
         gbc.gridx = 1;
         gbc.gridy = 2;
-        Panel.add(submit2,gbc);
-
+        Panel.add(submit2, gbc);
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 3;
-        Panel.add(TableTitle,gbc);
-
+        Panel.add(TableTitle, gbc);
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 4;
-        Panel.add(j,gbc);
-
+        Panel.add(j, gbc);
         JPanel controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
         controlPanel.add(Panel);
         frame.add(controlPanel);
         frame.setVisible(true);
-
         frame.setVisible(true);
     }
 
-    public void DoctorPage(){
+    public void DoctorPage() {
         int doctorID = 5;
         JFrame frame = new JFrame();
-        frame.setSize(500,300);
+        frame.setSize(700, 300);
         frame.setTitle("Symptom Tracker: Doctor View");
-
         JLabel header = new JLabel("Doctor View");
         JPanel DoctorPanel = new JPanel();
-
         JLabel addPatient = new JLabel("Add a Patient:");
-
         GridBagLayout layout = new GridBagLayout();
         DoctorPanel.setLayout(layout);
         GridBagConstraints gbc = new GridBagConstraints();
         JTextField patientFillIn = new JTextField();
-        JButton submit1 = new JButton("Submit");
-        submit.addActionListener(e -> {
+        new JButton("Submit");
+        JLabel TreatmentHeader = new JLabel("Select Treatement for Patient");
+        JLabel DateHeader = new JLabel("Enter Date in yyyy/dd/mm");
+        String[] Treatments = new String[]{"Drink Water", "Tylenol"};
+        JComboBox<String> Treatment = new JComboBox(Treatments);
+        String[] pateints = new String[]{"Max C", "Zach J"};
+        JComboBox<String> MyPatient = new JComboBox(pateints);
+        JTextField EndDate = new JTextField();
+        JButton submit2 = new JButton("Submit");
+        this.submit.addActionListener((e) -> {
             String patientID = "You selected " + patientFillIn.getText();
             System.out.println(patientID);
-            int result = InsertServiceList.INSERT_DOCTORFOR.ExecuteQuery(new Object[]{
-                doctorID, patientFillIn.getText()
-            });
-        System.out.println(InsertServiceList.INSERT_DOCTORFOR.codeMeaning(result));
+            int result = InsertServiceList.INSERT_DOCTORFOR.ExecuteQuery(new Object[]{doctorID, patientFillIn.getText()});
+            System.out.println(InsertServiceList.INSERT_DOCTORFOR.codeMeaning(result));
         });
-
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        submit2.addActionListener((e) -> {
+            String patientID = "You selected " + patientFillIn.getText();
+            System.out.println(patientID);
+            int result = InsertServiceList.INSERT_NEEDS.ExecuteQuery(new Object[]{MyPatient.getSelectedIndex(), Treatment.getSelectedIndex(), LocalDateTime.now(), Date.valueOf("2023-01-30")});
+            System.out.println(InsertServiceList.INSERT_NEEDS.codeMeaning(result));
+        });
+        gbc.fill = 2;
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 0;
-        DoctorPanel.add(header,gbc);
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        DoctorPanel.add(header, gbc);
+        gbc.fill = 2;
         gbc.ipady = 20;
         gbc.gridx = 0;
         gbc.gridy = 1;
-        DoctorPanel.add(addPatient,gbc);
-        gbc.ipady = 20;
+        DoctorPanel.add(addPatient, gbc);
+        gbc.ipady = 27;
         gbc.gridx = 0;
         gbc.gridy = 2;
-        DoctorPanel.add(patientFillIn,gbc);
+        DoctorPanel.add(patientFillIn, gbc);
         gbc.ipady = 20;
         gbc.gridx = 1;
         gbc.gridy = 2;
-        DoctorPanel.add(submit, gbc);
-
+        DoctorPanel.add(this.submit, gbc);
+        gbc.ipady = 20;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        DoctorPanel.add(TreatmentHeader, gbc);
+        gbc.ipady = 20;
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        DoctorPanel.add(DateHeader, gbc);
+        gbc.ipady = 20;
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        DoctorPanel.add(Treatment, gbc);
+        gbc.ipady = 20;
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        DoctorPanel.add(MyPatient, gbc);
+        gbc.ipady = 27;
+        gbc.ipadx = 100;
+        gbc.gridx = 2;
+        gbc.gridy = 4;
+        DoctorPanel.add(EndDate, gbc);
+        gbc.ipady = 20;
+        gbc.gridx = 5;
+        gbc.gridy = 4;
+        DoctorPanel.add(submit2, gbc);
         JPanel controlPanel = new JPanel();
         controlPanel.setLayout(new FlowLayout());
         controlPanel.add(DoctorPanel);
         frame.add(controlPanel);
         frame.setVisible(true);
-
         frame.setVisible(true);
     }
-
-
 }
 
 
