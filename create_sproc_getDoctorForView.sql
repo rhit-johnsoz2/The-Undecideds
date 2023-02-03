2c7c25dfdcc43f68815e032567a3037842cb3a4b
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-02 23:42:26 -0500

    Update create_sproc_getDoctorForView.sql

--- create_sproc_getDoctorForView.sql
+++ create_sproc_getDoctorForView.sql
@@ -9,7 +9,7 @@ BEGIN
 		Raiserror('Doctor can not be null', 14, 1)
 		Return 1
 	END
-	if(NOT EXISTS (SELECT P.ID FROM Person P WHERE P.role = 'DR'))
+	if(NOT EXISTS (SELECT P.ID FROM Person P WHERE P.role = 'DR' and P.ID = @doctorid))
 	BEGIN
 		Raiserror('Doctor is not in table', 14, 1)
 		Return 2
