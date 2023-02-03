637989c29775028252b355c51f17821f54b62ffb
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-02 23:49:13 -0500

    Update create_view_doctorsWithTreatment.sql

--- create_view_doctorsWithTreatment.sql
+++ create_view_doctorsWithTreatment.sql
@@ -2,7 +2,7 @@ Use SymptomTracker
 Go
 CREATE VIEW DoctorsWithTreatment
 AS
-SELECT Doctor.fname, Doctor.lname, Doctor.ID
+SELECT T.ID as TreatmentID, Doctor.fname, Doctor.lname, Doctor.ID
 FROM Person Doctor JOIN Performs P
 On Doctor.ID = P.doctorID
 JOIN Treatment T on P.treatmentID = T.ID
