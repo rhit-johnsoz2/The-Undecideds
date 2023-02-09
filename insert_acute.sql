f9a0875e539110e70953c75b8d92b7afcccbd841
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 02:05:20 -0500

    Update insert_acute.sql

--- insert_acute.sql
+++ insert_acute.sql
@@ -1,28 +1,28 @@
 Use SymptomTracker
 Go
 CREATE PROCEDURE InsertAcute
-	(@personID Integer, @symptomID Integer, @name Varchar(40),
-	 @severity Integer, @symptomtimestamp Timestamp)
+	(@personID Integer, @symptomID Integer,
+	 @severity Integer, @symptomtimestamp Timestamp = GETDATE)
 AS
 BEGIN
 	-- check to see if any of the parameters are null
-	IF(@personID is NULL or @symptomID is NULL or @name is NULL or @severity is NULL or @symptomtimestamp is NULL)
+	IF(@personID is NULL or @symptomID is NULL or @severity is NULL or @symptomtimestamp is NULL)
 	BEGIN
 		RAISERROR('Input arguments cannot be null', 14, 1)
 		Return 1
 	END
 	-- check to see if the person exists in the person database
-	IF(NOT EXISTS (SELECT * FROM Person WHERE ID = @personID))
+	IF(NOT EXISTS (SELECT * FROM Person WHERE Person.ID = @personID))
 	BEGIN
 		RAISERROR('Person not in the person database', 14, 1)
 		Return 2
 	END
 	-- check to see if the symptom exists in the symptom database
-	IF(NOT EXISTS (SELECT * FROM Symptom WHERE ID = @symptomID))
+	IF(NOT EXISTS (SELECT * FROM Symptom WHERE Symptom.ID = @symptomID))
 	BEGIN
-		-- insert the new symptom into the symptom database
-		INSERT INTO Symptom Values(@symptomID, @name)
+		RAISERROR('Symptom not in the person database', 14, 1)
+		Return 3
 	END
-INSERT INTO Acute Values(@personID, @symptomID, @name, @severity, @symptomtimestamp)
+INSERT INTO Acute Values(@personID, @symptomID, @severity, @symptomtimestamp)
 Return 0
 END
