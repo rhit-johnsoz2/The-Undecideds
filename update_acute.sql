907e13269ed5af1b78c2d70dae1a52702bf512e8
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 17:21:04 -0500

    Update update_acute.sql

--- update_acute.sql
+++ update_acute.sql
@@ -5,10 +5,10 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-ALTER procedure [dbo].[UpdateAcuteSymptom](@personID int, @symptomID int, @severity int, @time timestamp)
+ALTER procedure [dbo].[UpdateAcuteSymptom](@personID int, @symptomID int, @severity int, @date date)
 As
 Begin
-if(@personID is null or @symptomID is null or @severity is null or @time is null)
+if(@personID is null or @symptomID is null or @severity is null or @date is null)
 	Begin
 		raiserror('Input Arguments cannot be null', 14,1)
 		return 1;
@@ -18,14 +18,15 @@ if(@severity > 10)
 		raiserror('Severity is outside scope',14,1)
 		return 2;
 	End
-if(@time < GETDATE())
+if(@date < GETDATE())
 	Begin
 		raiserror('Time frame is not avaliable',14,1)
 		return 3;
 	End
 
 Update Acute
-	Set severity = @severity
+	Set severity = @severity,
+	    date = @date
 	Where personID = @personID and symptomID = @symptomID
 	return 0;
 End
