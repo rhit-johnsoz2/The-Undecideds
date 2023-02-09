a4ab54f786c70805a0147cd7dec22453c3fe8c06
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 17:59:14 -0500

    Update insert_doctorfor.sql

--- insert_doctorfor.sql
+++ insert_doctorfor.sql
@@ -1,6 +1,11 @@
-Use SymptomTracker
-Go
-CREATE PROCEDURE InsertDoctorFor
+USE [SymptomTracker]
+GO
+/****** Object:  StoredProcedure [dbo].[InsertDoctorFor]    Script Date: 2/9/2023 5:52:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[InsertDoctorFor]
 	(@doctorID Integer, @patientID Integer)
 AS
 BEGIN
@@ -11,37 +16,21 @@ BEGIN
 		Return 1
 	END
 	-- check to see if the doctor exists in the patient database
-	IF(NOT EXISTS (SELECT * FROM Person WHERE ID = @doctorID))
+	IF(NOT EXISTS (SELECT * FROM Person WHERE Person.ID = @doctorID and Person.role ='DR'))
 	BEGIN
 		RAISERROR('Doctor not in the person database', 14, 1)
 		Return 2
 	END
 	-- check to see if the patient exists in the person database
-	IF(NOT EXISTS (SELECT * FROM Person WHERE ID = @patientID))
+	IF(NOT EXISTS (SELECT * FROM Person WHERE Person.ID = @patientID and Person.role = 'PA'))
 	BEGIN
-		RAISERROR('Patient not in the person database', 14, 1)
+		RAISERROR('Patient not in the HCP database', 14, 1)
 		Return 3
 	END
 INSERT INTO DoctorFor Values(@doctorID, @patientID)
 Return 0
 END
 
---Testing no errors
 --DECLARE @status int
---EXEC @status = dbo.InsertDoctorFor @doctorID = 3, @patientID = 2
---SELECT @status
-
---Testing error code 1
---DECLARE @status int
---EXEC @status = dbo.InsertDoctorFor @doctorID = null, @patientID = 2
---SELECT @status
-
---Testing error code 2
---DECLARE @status int
---EXEC @status = dbo.InsertDoctorFor @doctorID = 40, @patientID = 2
---SELECT @status
-
---Testing error code 3
---DECLARE @status int
---EXEC @status = dbo.InsertDoctorFor @doctorID = 3, @patientID = 30
+--EXEC @status = InsertAcuteSymptoms('clifton', 'Curt', 'Clifton')
 --SELECT @status
