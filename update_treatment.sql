0dbb868aba75df01efb0a743a4062a2b3ff2cb5f
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 02:07:49 -0500

    Update update_treatment.sql

--- update_treatment.sql
+++ update_treatment.sql
@@ -1,12 +1,12 @@
 USE [SymptomTracker]
 GO
-/****** Object:  StoredProcedure [dbo].[UpdateTreatment]    Script Date: 2/2/2023 5:29:58 PM ******/
+/****** Object:  StoredProcedure [dbo].[UpdateTreatment]    Script Date: 2/9/2023 12:59:48 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-ALTER procedure [dbo].[UpdateTreatment](@treatmentID int,@cost decimal(19,4), @name varchar(40))
+ALTER procedure [dbo].[UpdateTreatment](@treatmentID int, @cost int, @name varchar(40))
 As
 Begin 
 
@@ -18,13 +18,13 @@ Begin
 
 	if not Exists(select * from Treatment where ID = @treatmentID)
 	Begin
-		RAISERROR('Treatment does not exisit and cannot be updated',14,1)
+		RAISERROR('Treatment does not exist and cannot be updated',14,1)
 		return 2;
 	End
 
 	Update Treatment
-		Set name = @name,
-			Cost = @cost
+		Set	Cost = @cost,
+		    name = @name
 		where ID = @treatmentID
 		return 0;
 End
