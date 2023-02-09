85bc80a6b86d4c4a3f85b66f4a72c0a782085ac3
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 02:27:32 -0500

    Update insert_sideEffects.sql

--- insert_sideEffects.sql
+++ insert_sideEffects.sql
@@ -1,6 +1,11 @@
-use [SymptomTracker]
-go
-create procedure InsertSideEffectOf(@SymptomID int, @TreatmentID int)
+USE [SymptomTracker]
+GO
+/****** Object:  StoredProcedure [dbo].[InsertSideEffectOf]    Script Date: 2/9/2023 2:26:13 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER procedure [dbo].[InsertSideEffectOf](@SymptomID int, @TreatmentID int)
 As
 Begin
 	if(@SymptomID is null or @TreatmentID is null)
@@ -8,6 +13,16 @@ Begin
 		raiserror('Input Arguments cannot be null', 14,1)
 		return 1
 	End
-	Insert into dbo.SideEffectsOf(SymptomID, TreatmentID) Values(@SymptomID, @TreatmentID)
+	if not exists(select * from Symptom where Symptom.ID = @SymptomID)
+	Begin
+		raiserror('Symptom does not exist', 14,1)
+		return 2
+	End
+	if not exists(select * from Treatment where Treatment.ID = @TreatmentID)
+	Begin
+		raiserror('Treatment does not exist', 14,1)
+		return 3
+	End 
+	Insert into dbo.SideEffectOf(SymptomID, TreatmentID) Values(@SymptomID, @TreatmentID)
 	return 0
 End
