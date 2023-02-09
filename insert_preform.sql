8ffd8489745fdd6ebf060ab965d60cade3479209
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 02:22:51 -0500

    Update insert_preform.sql

--- insert_preform.sql
+++ insert_preform.sql
@@ -1,22 +1,32 @@
-use [SymptomTracker]
-go
+USE [SymptomTracker]
+GO
+/****** Object:  StoredProcedure [dbo].[InsertPreform]    Script Date: 2/9/2023 2:18:02 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
 
-create procedure InsertPreform(@doctorID Integer,@treatmentID Integer)
+ALTER procedure [dbo].[InsertPreform](@doctorID Integer, @treatmentID Integer)
 AS
 Begin
-	if(@doctorID is null or @TreatmentID is null)
+	if(@doctorID is null or @treatmentID is null)
 	Begin
 		raiserror('Input Arguments cannot be null', 14,1)
 		return 1
 	End
 
-	if exists(select * from Performs where TreatmentID = @TreatmentID)
+	if not exists(select * from Treatment where Treatment.ID = @treatmentID)
 	Begin
-		raiserror('Input Arguments already exists in Preform', 14,1)
+		raiserror('Treatment does not exist', 14,1)
 		return 2
 	End 
-	Insert into dbo.Preforms(doctorID, TreatmentID) Values(@doctorID,@TreatmentID)
+
+	if not exists(select * from Person where Person.ID = @doctorID and Person.role = 'DR')
+	Begin
+		raiserror('Doctor does not exist', 14,1)
+		return 2
+	End
+
+	Insert into dbo.Preforms(doctorID, treatmentID) Values(@doctorID, @treatmentID)
 	return 0
 End
-
-
