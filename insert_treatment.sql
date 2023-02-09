85db32cb73f965ac979e4a9c2ec408e6b5abca78
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-09 02:28:41 -0500

    Update insert_treatment.sql

--- insert_treatment.sql
+++ insert_treatment.sql
@@ -1,6 +1,6 @@
 use [SymptomTracker]
 go
-create procedure InsertTreatment(@ID int, @Cost int, @name varchar(30))
+alter procedure InsertTreatment(@ID int, @Cost int, @name varchar(30))
 As
 Begin
 	if(@ID is null or @Cost is null or @name is null)
@@ -8,12 +8,6 @@ Begin
 		raiserror('Input Arguments cannot be null', 14,1)
 		return 1
 	End
-
-	if exists(select * from Treatment where ID = @ID)
-	Begin
-		raiserror('Input Arguments already exists in Treatment', 14,1)
-		return 2
-	End 
 	Insert into dbo.Treatment(ID, Cost, name) Values(@ID, @Cost, @name)
 	return 0
 End
