3afa61e7bc198bd08fe826723af25ef3e2c9247a
rhit-johnsoz2 <89936098+rhit-johnsoz2@users.noreply.github.com>
2023-02-17 05:12:50 -0500

    Update MigrateSymptomTracker.sql

--- MigrateSymptomTracker.sql
+++ MigrateSymptomTracker.sql
@@ -1483,3 +1483,30 @@ AS
 	END
 	SELECT ID, Name FROM NotTreatmentForView WHERE DoctorID = @doctorID
 GO
+
+CREATE PROCEDURE InsertNeedsNoEnd
+	(@patientID Integer, @treatmentID Integer, @sDate Date)
+AS
+BEGIN
+	-- check to see if any of the parameters are null
+	IF(@patientID is NULL or @treatmentID is NULL or @sDate is NULL)
+	BEGIN
+		RAISERROR('Input arguments cannot be null', 14, 1)
+		Return 1
+	END
+	-- check to see if the person exists in the person database
+	IF(NOT EXISTS (SELECT * FROM Person WHERE ID = @patientID))
+	BEGIN
+		RAISERROR('Patient not in the person database', 14, 1)
+		Return 2
+	END
+	-- check to see if the treatment exists in the treatment database
+	IF(NOT EXISTS (SELECT * FROM Treatment WHERE ID = @treatmentID))
+	BEGIN
+		RAISERROR('Treatment not in the treatment database', 14, 1)
+		Return 3
+	END
+INSERT INTO Needs Values(@patientID, @treatmentID, @sDate, null)
+Return 0
+END
+GO
